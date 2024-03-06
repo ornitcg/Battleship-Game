3e06473fcd4624e13e517a340e215e6673ffe4ea
OCG <40301816+ornitcg@users.noreply.github.com>
2024-03-07 01:49:50 +0200

    added activities for OptionsActivity  and GameRooms  not functional yet

---
+++ app/src/main/java/com/clientapp/battlshipclient/GameRooms.java
@@ -0,0 +1,14 @@
+package com.clientapp.battlshipclient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GameRooms extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_rooms);
+    }
+}
--- app/src/main/java/com/clientapp/battlshipclient/OptionsActivity.java
+++ app/src/main/java/com/clientapp/battlshipclient/OptionsActivity.java
@@ -6,20 +6,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class SignUpSignInActivity extends AppCompatActivity {
-
-    private boolean playBackOn = true;
+public class OptionsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up_sign_in);
-
+        setContentView(R.layout.activity_options);
     }
 
 
-    public void goToOptionsActivity(View v) {
-        Intent intent = new Intent(SignUpSignInActivity.this, OptionsActivity.class);
+    public void goToGameRooms(View v) {
+        Intent intent = new Intent(OptionsActivity.this, GameRooms.class);
         startActivity(intent);
     }
 }
