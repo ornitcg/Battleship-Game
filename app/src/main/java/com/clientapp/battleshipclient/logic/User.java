162fdfb778f515df445a98240902c8ef3c236a67
ornitcg <ornit.cgindi@gmail.com>
2024-03-27 23:51:14 +0200

    updated user

--- app/src/main/java/com/clientapp/battleshipclient/logic/User.java
+++ app/src/main/java/com/clientapp/battleshipclient/logic/User.java
@@ -1,5 +1,7 @@
 package com.clientapp.battleshipclient.logic;
 
+import android.util.Log;
+
 import com.clientapp.battleshipclient.networking.UserNW;
 
 public class User {
@@ -12,28 +14,35 @@ public class User {
         this.password = password;
     }
 
+    public String getUserID() {
+        UserNW userNW = new UserNW();
+        userNW.getUserID(this);
+        return "MyId";
+    }
 
 
     public enum UserState {
-        SIGNUP, SIGNIN, SIGNUP_SUCCEDED, SIGNIN_SUCCEDED, VALID,
+        SIGNUP_SUCCEDED, SIGNIN_SUCCEDED, VALID,
         USER_EXISTS, SIGNIN_FAILED ,USER_NAME_EXISTS, INVALID_USERNAME, INVALID_PASSWORD
     }
 
     public UserState signUp(){
-        UserState userStatus = isValidUser(this);  // ********** need new name for isValidUser ************
-        if (userStatus == UserState.VALID) {
-            UserNW userNW = new UserNW();
-            userNW.addUser(this);  // requires check if successful
-            return UserState.SIGNUP_SUCCEDED;
-        }
-        return userStatus;
+        UserState userStatus = isValidUser(this);
+        Log.d("User", "signUp: " + userStatus);
+        if (userStatus != UserState.VALID)
+            return userStatus;
+        UserNW userNW = new UserNW();
+//        return userNW.signUp(this);
+        return UserState.SIGNUP_SUCCEDED;
     }
 
     public UserState signIn() {
-        UserNW  userNW = new UserNW();
-        if (isInList(this))
-            return UserState.SIGNIN_SUCCEDED;
-        return UserState.SIGNIN_FAILED;
+        UserState userStatus = isValidUser(this);
+        if (userStatus != UserState.VALID)
+            return userStatus;
+        UserNW userNW = new UserNW();
+//        return userNW.signIn(this);  // send userName and password to server via sign in endpoint
+        return UserState.SIGNIN_SUCCEDED;
     }
 
     public String getUsername() {
@@ -48,26 +57,17 @@ public class User {
 
     private UserState isValidUser(User user) {
         UserNW userNW = new UserNW();
-
-        if (username.length() ==0 || username.length() >10)
+        Log.d("User", "isValidUser: " + username );
+        if (username.isEmpty() || username.length() >10)
             return UserState.INVALID_USERNAME;
-        if (password.length() ==0 || password.length() >10)
+        if (password.isEmpty() || password.length() >10)
             return UserState.INVALID_PASSWORD;
-        if (userNW.getUsers().contains(user))
-            return UserState.USER_EXISTS;
-        if (isNameUsed(userNW)) {
-            return UserState.USER_NAME_EXISTS;
-        }
+        Log.d("User", "isValidUser: " + User.UserState.VALID);
         return UserState.VALID;
     }
 
 
-    private boolean isNameUsed(UserNW userNW) {
-        for (User u : userNW.getUsers())
-             if (u.getUsername().equals(username))
-                 return true;
-        return false;
-    }
+
 
 
 
