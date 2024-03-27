2804cd3674fb3a098c2fc9430e042c6e8946f4a5
ornitcg <ornit.cgindi@gmail.com>
2024-03-27 23:52:26 +0200

    updated signup/signin activity

--- app/src/main/java/com/clientapp/battleshipclient/view/activities/SignUpSignInActivity.java
+++ app/src/main/java/com/clientapp/battleshipclient/view/activities/SignUpSignInActivity.java
@@ -2,6 +2,7 @@ package com.clientapp.battleshipclient.view.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,69 +19,17 @@ import com.clientapp.battleshipclient.utils.AudioUtils;
 public class SignUpSignInActivity extends BaseActivity {
 
     private User currentPlayer;
+    private Button signUpButton; // for easy access of methods
+    private Button signInButton; // for easy access of methods
+    private String currentPlayerUserId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_sign_in);
         setupMusicToggleButton(this);
-
-        String userNameEditText = findViewById(R.id.userNameInputViewId).toString();
-        String passwordEditText = findViewById(R.id.userPasswordInputViewId).toString();
-        User user = new User(userNameEditText, passwordEditText);
-        Button signUpButton = findViewById(R.id.signUpBtnId);
-        Button signInButton = findViewById(R.id.signInBtnId);
-
-        signUpButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                User.UserState userState = user.signUp();
-                switch (userState) {
-                    case SIGNUP_SUCCEDED:
-                        Toast.makeText(SignUpSignInActivity.this, "Sign Up Successful", Toast.LENGTH_SHORT).show();
-                        goToOptionsActivity(v);
-                        break;
-                    case USER_EXISTS:
-                        Toast.makeText(SignUpSignInActivity.this, "User exists, go to Sign In", Toast.LENGTH_SHORT).show();
-                        break;
-                    case INVALID_USERNAME:
-                        Toast.makeText(SignUpSignInActivity.this, "Invalid Username", Toast.LENGTH_SHORT).show();
-                        break;
-                    case INVALID_PASSWORD:
-                        Toast.makeText(SignUpSignInActivity.this, "Invalid Password", Toast.LENGTH_SHORT).show();
-                        break;
-                    case USER_NAME_EXISTS:
-                        Toast.makeText(SignUpSignInActivity.this, "Username already exists", Toast.LENGTH_SHORT).show();
-                        break;
-                    default:
-                        Toast.makeText(SignUpSignInActivity.this, "Sign Up Failed", Toast.LENGTH_SHORT).show();
-                        break;
-                }
-
-            }
-        });
-
-        signInButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(SignUpSignInActivity.this, "Sign In Button Clicked", Toast.LENGTH_SHORT).show();
-                User.UserState userState = user.signIn();
-                switch (userState) {
-                    case SIGNIN_SUCCEDED:
-                        Toast.makeText(SignUpSignInActivity.this, "Sign In Successful", Toast.LENGTH_SHORT).show();
-                        goToOptionsActivity(v);
-                        break;
-                    case SIGNIN_FAILED:
-                        Toast.makeText(SignUpSignInActivity.this, "Sign In Failed", Toast.LENGTH_SHORT).show();
-                        break;
-                    default:
-                        Toast.makeText(SignUpSignInActivity.this, "Sign In Failed", Toast.LENGTH_SHORT).show();
-                        break;
-                }
-            }
-        });
-
-
+        setProperties(); // fill in the class properties
+        setButtonsListeners(); // set the buttons listeners
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -89,10 +38,78 @@ public class SignUpSignInActivity extends BaseActivity {
         });
     }
 
+    private void setProperties() {
+        signUpButton = findViewById(R.id.signUpBtnId);
+        signInButton = findViewById(R.id.signInBtnId);
+
+    }
+
+    private void setButtonsListeners() {
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText userNameInputView =  findViewById(R.id.userNameInputViewId);
+                EditText userPasswordInputView = findViewById(R.id.userPasswordInputViewId);
+                String userName = userNameInputView.getText().toString();
+                String password = userPasswordInputView.getText().toString();
+                currentPlayer = new User(userName, password);
+                User.UserState userState = currentPlayer.signUp();
+                switch (userState) {
+                    case SIGNUP_SUCCEDED:
+                        setUserId(currentPlayer.getUserID());
+                        goToOptionsActivity(v);
+                        break;
+                    case USER_EXISTS:
+                        Toast.makeText(SignUpSignInActivity.this, "User Exists, go to SIGN IN", Toast.LENGTH_SHORT).show();
+                        break;
+                    case USER_NAME_EXISTS:
+                        Toast.makeText(SignUpSignInActivity.this, "User Name Exists", Toast.LENGTH_SHORT).show();
+                        break;
+                    case INVALID_USERNAME:
+                        Toast.makeText(SignUpSignInActivity.this, "Invalid Username", Toast.LENGTH_SHORT).show();
+                        break;
+                    case INVALID_PASSWORD:
+                        Toast.makeText(SignUpSignInActivity.this, "Invalid Password", Toast.LENGTH_SHORT).show();
+                        break;
+                    default:
+                        Toast.makeText(SignUpSignInActivity.this, "Sign Up Failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText userNameInputView =  findViewById(R.id.userNameInputViewId);
+                EditText userPasswordInputView = findViewById(R.id.userPasswordInputViewId);
+                String userName = userNameInputView.getText().toString();
+                String password = userPasswordInputView.getText().toString();
+                Log.d("User", "USERNAME: " + userName);
+                currentPlayer = new User(userName, password);
+                User.UserState userState = currentPlayer.signIn();
+                switch (userState) {
+                    case SIGNIN_SUCCEDED:
+                        setUserId(currentPlayer.getUserID());
+                        goToOptionsActivity(v);
+                        break;
+                    case SIGNIN_FAILED:
+                        Toast.makeText(SignUpSignInActivity.this, "Sign In Failed", Toast.LENGTH_SHORT).show();
+                        break;
+                    default:
+                        Toast.makeText(SignUpSignInActivity.this, "Sign In Failed", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+    }
+
+    private void setUserId(String userID) {
+        this.currentPlayerUserId = userID;
+    }
 
     public void goToOptionsActivity(View v) {
-        Intent intent = new Intent(SignUpSignInActivity.this, PlaceYourShips.class);
+        Intent intent = new Intent(SignUpSignInActivity.this, OptionsActivity.class);
         intent.putExtra("buttonSound", R.raw.enter_button_sound);
+        intent.putExtra("currPlayerUserId", currentPlayer.getUserID());
         startActivity(intent);
     }
 
