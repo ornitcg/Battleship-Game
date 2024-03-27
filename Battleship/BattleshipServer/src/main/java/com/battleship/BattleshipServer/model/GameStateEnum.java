20be2f395ccfacca45fbb620584513889f11d657
Yuval Gabai <97031980+yuvalg4@users.noreply.github.com>
2024-03-27 12:31:07 +0200

    Delete Battleship/BattleshipServer directory

--- Battleship/BattleshipServer/src/main/java/com/battleship/BattleshipServer/model/GameStateEnum.java
+++
@@ -1,9 +0,0 @@
-package com.battleship.BattleshipServer.model;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public enum GameStateEnum {
-    @JsonProperty("inProgress") IN_PROGRESS,
-    @JsonProperty("active") ACTIVE,
-    @JsonProperty("finished") FINISHED,
-}
--- Battleship/BattleshipServer/src/main/java/com/battleship/BattleshipServer/resources/GameResource.java
+++
@@ -1,9 +0,0 @@
-package com.battleship.BattleshipServer.resources;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class GameResource {
-
-
-}
